e034aee10da1cd9d503d8d2ba9ce4f4e193ad1ee
birkholz-cubert <birkholz@cubert-gmbh.de>
2025-05-22 12:13:52 +0200

    wip

--- build_docs.sh
+++ build_docs.sh
@@ -16,7 +16,6 @@ cmake  -DCMAKE_BUILD_TYPE=Release -B ./_pyil cuvis.pyil
 cmake --build "_pyil" --target cuvis_pyil --config Release
 cp _pyil/_cuvis_pyil.so ./cuvis.pyil/cuvis_il
 cp _pyil/cuvis_il.py ./cuvis.pyil/cuvis_il
-cat _pyil/cuvis_il.py
 cd cuvis.pyil
 python3.10 -m pip install .
 cd ../cuvis.python
@@ -28,10 +27,10 @@ echo "Build Documentation"
 echo "======================="
 
 source venv/bin/activate
-mkdir _build
+mkdir -p _build
 cmake -B _build .
 cmake --build "_build"
-python3.10 -m sphinx -b html -Dbreathe_projects.cuvis=_build/doc/xml ./docs ./_build
-touch ./_build/doc/sphinx/.nojekyll
+python3.10 -m sphinx -b html -Dbreathe_projects.cuvis=_build/doc/xml docs _build
+touch _build/doc/sphinx/.nojekyll
 
 
