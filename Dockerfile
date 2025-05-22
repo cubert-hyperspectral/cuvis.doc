c40db62ec8cfec1685bc5f2fb7c8e740a1e564b9
birkholz-cubert <birkholz@cubert-gmbh.de>
2025-05-22 12:05:20 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM cubertgmbh/cuvis_base:3.3.3-ubuntu22.04
 
-
 RUN apt-get update && \
-    apt-get install doxygen swig -y && \
-	apt-get install python3.10 python3.10-dev python3.10-venv -y
+    apt-get install curl gcc g++ make swig doxygen -y && \
+	apt-get install python3.10 python3.10-dev python3.10-venv python3-pip -y
 
 WORKDIR /tmp
 
--- build_docs.sh
+++ build_docs.sh
@@ -3,19 +3,16 @@
 
 cd /app
 
-python3.10 -m venv venv
-source venv/bin/activate
 python3.10 -m pip install -r docs/requirements.txt
 
 echo "======================="
 echo "Build Python wrapper Wrapper"
 echo "======================="
 
-source venv/bin/activate
-python --version
+python3.10 --version
 python3.10 -m pip install numpy==1.22.0
-mkdir _pyil
-cmake  -DCMAKE_BUILD_TYPE=Release -B ./_pyil -DPython_ROOT_DIR=./venv cuvis.pyil
+mkdir -p _pyil
+cmake  -DCMAKE_BUILD_TYPE=Release -B ./_pyil cuvis.pyil
 cmake --build "_pyil" --target cuvis_pyil --config Release
 cp _pyil/_cuvis_pyil.so ./cuvis.pyil/cuvis_il
 cp _pyil/cuvis_il.py ./cuvis.pyil/cuvis_il
