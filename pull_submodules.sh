b9c84ad4415d8a47754515770c09c5faab0e3c9a
birkholz-cubert <birkholz@cubert-gmbh.de>
2025-06-30 10:45:47 +0200

    added helper script

---
+++ pull_submodules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd cuvis.c
+git pull --ff-only
+cd ..
+cd cuvis.c.examples
+git pull --ff-only
+cd ..
+cd cuvis.cpp
+git pull --ff-only
+cd ..
+cd cuvis.cpp.examples
+git pull --ff-only
+cd ..
+cd cuvis.pyil
+git pull --ff-only
+cd ..
+cd cuvis.python
+git pull --ff-only
+cd ..
+cd cuvis.python.examples
+git pull --ff-only
+cd ..
